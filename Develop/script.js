7a11656bf0d1b6120e5cc6221472a2d7b2332e16
catcueto <catacueto@gmail.com>
2022-06-29 17:58:16 -0400

    generated password is displaying

--- Develop/script.js
+++ Develop/script.js
@@ -1,41 +1,18 @@
 // Assignment Code
 // This variable targets the generate id
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// USE THIS AS GUIDELINES
-// function generatePassword() {
-//   var length = 8,
-//       charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-//       retVal = "";
-//   for (var i = 0, n = charset.length; i < length; ++i) {
-//       retVal += charset.charAt(Math.floor(Math.random() * n));
-//   }
-//   return retVal;
-// }
-
-// MY CODE
-// This function definepus generate Password
+// This function defines generate Password
 function generatePassword() {
   console.log("Hola, bola! This is working");
 
+  // MY CODE
   // STEP 1 : PROMPT THE USER FOR THE PASSWORD CRITERIA//
   // a) Password length: 8 < 128 characters
   var passwordLength = window.prompt(
     "Please enter the number of characters you would like your password to have. It must be between 8 and 128 characters"
   );
   if (passwordLength < 8 || passwordLength > 128) {
-    return "Invalid password length. Click the burgundy button below to restart the generator";
+    return "Invalid password length. Click the burgundy button below to restart the generator.";
   } else {
     console.log("Valid password length selected");
   }
@@ -57,20 +34,25 @@ function generatePassword() {
 
   // c) Variabels should include lowercase, uppercase, numbers, and special characters
 
-  // create empty strings
-  var charSet = "";
   // If user selects [CANCEL] for all the prompts...
   if (!lowerCase && !upperCase && !numbers && !symbols) {
+    // return this...
     return "You didn't select the required character criteria. Try again! :)";
   }
 
+  // STEP 2: VALIDATE THE INPUT
+  // create empty string
+  var charSet = "";
+  console.log(charSet);
   // THIS IS WHERE WE DEFINE OUR VARIABLES
-  // If our lowerCase variable is selected [OK], then console.log...
+  // If our lowerCase value(s) is selected [OK], then console.log...
   if (lowerCase) {
     charSet += "abcdefghijklmnopqrstuvwxyz";
     console.log("Lower-case character(s) has been added");
   }
   if (upperCase) {
+    //  // If our upper-case values(s) is selected [OK], then console.log...
+
     charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     console.log("Upper-case character has been added");
   }
@@ -82,31 +64,32 @@ function generatePassword() {
     charSet += "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
     console.log("Special character(s) has been added");
   }
+
+  //returns the length of variable charSet
+  var charLength = charSet.length;
+
+  // STEP 3: GENERATING RANDOM PASSWORD BASED ON CRITERIA ABOVE
   var newPassword = "";
+  for (var i = 0; i < passwordLength; i++) {
+    // Math.floor rounds a number DOWN to the nearest integer
+    // Math.random creates a number between 0 and 1
+    var character = Math.floor(Math.random() * charLength);
+    // charAt gets the first character in a string
+    newPassword += charSet.charAt(character);
+  }
+
+  // STEP 4: DISPLAY RANDOMIZED PASSWORD ON SCREEN
+  // Need to return function to display something
+  return newPassword;
 }
 
-// STEP 2: VALIDATE THE INPUT
-console.log(charSet);
-//returns the length of variable charSet
-var charLength = charSet.length;
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-// STEP 3
-// define all possible variables for characters
-// var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-// var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// var numbers = "0123456789";
-// var symbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+  passwordText.value = password;
+}
 
-// Window pprompt - returns input value if the user click "OK", otherwise null
-
-//   var paswordLength = function (min, max) {
-//     8, 128;
-//     var password = "";
-//   };
-
-//   for (var = 0, i <= passwordLength; i++){
-//     var randomNumber = Math.floor(Math.random()* chars.length);
-//     password += chars.substring(randomNumber, randomNumber+1)
-//   }
-//   return "hiya";
-// }
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
